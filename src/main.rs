c4bb19e019831ed5ab89922d205e76570b857786
torressam333 <torressam333@gmail.com>
2025-12-18 08:14:12 -0800

    Need to implement new method in index rs to complete indexing tests

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod search;
 mod tokenizer;
 mod watcher;
 use clap::{Parser, Subcommand};
+use std::{path::PathBuf, time::SystemTime};
+use uuid::Uuid;
 
 use crate::tokenizer::tokenize;
 
@@ -41,4 +43,19 @@ fn run_search(query: String) {
     let tokens = tokenize(&query);
 
     println!("tokens from query ={:#?}", tokens);
+
+    // Create new index
+    let mut index = index::Index::new();
+
+    // mock doc for now
+    let mock_doc = ingestion::Document {
+        id: Uuid::new_v4(),
+        path: PathBuf::new(),
+        content: "Hi There!".to_string(),
+        modified: Some(SystemTime::now()),
+    };
+
+    index.add_document(&mock_doc);
+
+    index.search_query(&query); // ->>> doesnt exist implement in index.rs
 }
